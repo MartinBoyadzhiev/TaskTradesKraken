b114ba75da09fac8b0279bf7e08dcda8c711cd03
martin.boyadzhiev <martin.boqdjiev00@gmail.com>
2025-08-07 16:14:14 +0300

    Setting up the app to work with environment variables.

--- src/main/java/MainConnectionTest.java
+++ src/main/java/MainConnectionTest.java
@@ -2,25 +2,25 @@ import com.google.gson.Gson;
 import dto.KrakenSubscription;
 
 import java.net.URISyntaxException;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
 import java.util.logging.Logger;
 
 public class MainConnectionTest {
 
-    private static final String BASE_WS_URL = "wss://ws.kraken.com/";
-    private static final int DEPTH_LIMIT = 100;
+    private static final String BASE_WS_URL = System.getenv("KRAKEN_WS_URL");
+    private static final int DEPTH_LIMIT = Integer.parseInt(System.getenv("DEPTH_LIMIT"));
+    private static final int VWAP_AMOUNT = Integer.parseInt(System.getenv("VWAP_AMOUNT"));
     private static final Gson gson = new Gson();
     public static final Logger logger = Logger.getLogger("MainConnectionTest");
 
     public static void main(String[] args) throws URISyntaxException, InterruptedException {
         OrderBookManager bookManager = new OrderBookManager(DEPTH_LIMIT);
-//        Hardcode test
-        String pair1 = "ETH/USD";
-        String pair2 = "SOL/USD";
-        String pair3 = "XRP/USD";
 
-        KrakenSubscription krakenSubscription = new KrakenSubscription(List.of(pair2, pair1, pair3), DEPTH_LIMIT);
+        List<String> subscriptionPairs = Arrays.stream(System.getenv("KRAKEN_PAIR").split(",")).toList();
+
+        KrakenSubscription krakenSubscription = new KrakenSubscription(subscriptionPairs, DEPTH_LIMIT);
         String sub = gson.toJson(krakenSubscription);
 
         WebSocketConnector ws = new WebSocketConnector(BASE_WS_URL, sub, bookManager);
@@ -41,8 +41,8 @@ public class MainConnectionTest {
                     continue;
                 }
                 System.out.println(pair + " mid price is " + book.getMidPrice());
-                System.out.println(pair + " asks VWAP price is " + book.calculateVWAPAsks(10));
-                System.out.println(pair + " bids VWAP price is " + book.calculateVWAPAsks(10));
+                System.out.println(pair + " asks VWAP price is " + book.calculateVWAPAsks(VWAP_AMOUNT));
+                System.out.println(pair + " bids VWAP price is " + book.calculateVWAPAsks(VWAP_AMOUNT));
             }
         }
     }
